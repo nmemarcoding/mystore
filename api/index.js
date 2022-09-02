19d5bc6180e2039e1c9e8fb05d9caed71b262905
nmemarcoding <nmemarcoding@gmail.com>
2022-09-02 11:22:44 -0700

    inStock added to product models

--- api/index.js
+++ api/index.js
@@ -13,11 +13,11 @@ const cors = require("cors");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +28,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+app.listen(process.env.PORT || 5001, () => {
+    console.log("Backend server is running!");
 });
